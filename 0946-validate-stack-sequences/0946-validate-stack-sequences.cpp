e3077a5506b3d78740bf59929923c965f8e1b3e7
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2023-04-13 08:14:32 +0530

    Time: 11 ms (52.74%), Space: 15.4 MB (24.28%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -4,10 +4,8 @@ public:
         stack<int> st;
         int j = 0;
         for(int i=0; i<pushed.size(); i++){
-            cout<<"PUSH: "<<pushed[i]<<endl;
             st.push(pushed[i]);
             while(!st.empty() && st.top() == popped[j] && j<popped.size()){
-                cout<<"POP: "<<st.top()<<endl;
                 st.pop();
                 j++;
             }
