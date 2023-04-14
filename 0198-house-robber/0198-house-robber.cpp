981ba2d4b421eae210b300f1bbd03b1c980b972d
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2023-04-14 12:56:50 +0530

    Time: 2 ms (47.42%), Space: 7.7 MB (86.71%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -6,13 +6,12 @@ public:
             return nums[0];
         }
         
-        vector<int> dp(nums.size() + 1);
         int ans;
         int back = 0;
         int prev = nums[0];
-        for(int i=2; i<dp.size(); i++){
+        for(int i=1; i<nums.size(); i++){
             int notPick = prev;
-            int pick = nums[i-1] + back;
+            int pick = nums[i] + back;
             ans = max(pick, notPick);
             back = prev;
             prev = ans;
