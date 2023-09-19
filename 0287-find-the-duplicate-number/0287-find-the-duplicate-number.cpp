5b1d887070d9efe4c3d80a3c49ccd895af493241
avinashsdhakne <110440978+avinashsdhakne@users.noreply.github.com>
2023-09-19 15:09:01 +0530

    Time: 80 ms (74.03%), Space: 61.5 MB (28.42%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -3,13 +3,11 @@ public:
     int findDuplicate(vector<int>& nums) {
         int leader = nums[0];
         while(1){
-            cout<<leader<<" "<<nums[leader]<<endl;
             if(nums[leader] == 0) return leader;
             int temp = leader;
             leader = nums[leader];
             nums[temp] = 0;
         }
-        
         return 0;
     }
 };
